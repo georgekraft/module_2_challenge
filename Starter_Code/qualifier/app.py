8f8f4f56285c252a07133bc972303dfca9127988
georgekraft <85372363+georgekraft@users.noreply.github.com>
2021-07-01 18:37:24 -0700

    Update CLI

--- Starter_Code/qualifier/app.py
+++ Starter_Code/qualifier/app.py
@@ -118,7 +118,7 @@ def run():
         bank_data, credit_score, debt, income, loan_amount, home_value
     )
 
-    # Save qualifying loans
+    # Save qualifying loans, see module qualifier/utils/save_qualifying_loans.py
     save_qualifying_loans(qualifying_loans)
 
 
--- Starter_Code/qualifier/qualifier/utils/save_qualifying_loans.py
+++ Starter_Code/qualifier/qualifier/utils/save_qualifying_loans.py
@@ -1,3 +1,9 @@
+import sys
+import fire
+import questionary
+from pathlib import Path
+import csv
+
 def save_qualifying_loans(qualifying_loans):
     """Saves the qualifying loans to a CSV file.
         Modular function imported into app.py
@@ -6,8 +12,15 @@ def save_qualifying_loans(qualifying_loans):
         qualifying_loans (list of lists): The qualifying bank loans.
     """
     # @TODO: Complete the usability dialog for savings the CSV Files.
-    csvpath = Path("qualifying_loans.csv")
-    with open(csvpath, 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        for row in qualifying_loans:
-            csvwriter.writerow(row.values())
+    
+    answer = questionary.text("Would you like to save your qualifying loans?").ask()
+
+    if answer == 'Yes' or 'yes' or 'YES' or 'Y' or 'y':
+        file_path = questionary.text("Enter a file path to save_qualifying_loans():").ask()
+        csvpath = Path(file_path)
+        with open(csvpath, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            for row in qualifying_loans:
+                csvwriter.writerow(row)
+    else:
+        print("Since you didn't answer Yes to saving your qualifying loans, they have not been saved.")
